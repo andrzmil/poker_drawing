eccf78dea5c53ef782c624fa9356f229d0850ca5
Andrzej Milewski <andrz.milewski@gmail.com>
2020-09-13 21:24:41 +0200

    new approach: numbers first instead of colors, added numbers to figure mapping
    Signed-off-by: Andrzej Milewski <andrz.milewski@gmail.com>

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
 import numpy as np
-import pandas as pd
-from tkinter import messagebox
-
-
 
 #built card deck by using dictionary
-deck = {'H':[2,3,4,5,6,7,8,9,10,11,12,13,14], 'C':[2,3,4,5,6,7,8,9,10,11,12,13,14],
-        'D':[2,3,4,5,6,7,8,9,10,11,12,13,14], 'S':[2,3,4,5,6,7,8,9,10,11,12,13,14]}
+deck = {'2':['H','C','S','D'], '3':['H','C','S','D'], '4':['H','C','S','D'],
+        '5':['H','C','S','D'], '6':['H','C','S','D'], '7':['H','C','S','D'],
+        '8':['H','C','S','D'], '9':['H','C','S','D'], '10':['H','C','S','D'],
+        '11':['H','C','S','D'], '12':['H','C','S','D'], '13':['H','C','S','D'],
+        '14':['H','C','S','D']}
 
 def drawing_process():
-    colors = deck.keys()
+    #defines how many numbers are currently in deck
+    numbers = deck.keys()
 
-    def random_color():
-        return np.random.randint(1, len(colors)+1)
-
-    chosen_color = list(colors)[random_color()-1]
-    numbers_length = len(deck.get(chosen_color))
-
+    #draw number which represent number in deck
     def random_number():
-        return np.random.randint(2, numbers_length+2)
-
-    temp_deck_check = pd.DataFrame.from_dict(deck, orient="index")
-    temp_deck_check.to_csv('check.csv', mode='a')
-
+        return str(np.random.randint(2, len(numbers)+2))
 
     chosen_number = random_number()
+    numbers_length = len(deck.get(chosen_number))
+
+    def random_color():
+        return np.random.randint(0, numbers_length)
+
+    chosen_color = deck.get(chosen_number)[random_color()]
+    deck[str(chosen_number)].remove(chosen_color)
+
+    return [chosen_number, chosen_color]
+
+#convert numbers to correct card figures (J,Q,K,A)
 
 
-    deck[chosen_color].remove(chosen_number)
-
-
-
-
-
-    return [chosen_color, chosen_number]
-
-
-
+#draw player and board cards
 player_one = [drawing_process(), drawing_process()]
 player_two = [drawing_process(), drawing_process()]
-
-
 board = [drawing_process(), drawing_process(), drawing_process(), drawing_process(), drawing_process()]
 
+def change_figures(list_cards):
+    new_list_cards = list_cards
+    for list in new_list_cards:
+        for num, element in enumerate(list):
+            if element == "11":
+                list[num] = "J"
+            if element == "12":
+                list[num] = "Q"
+            if element == "13":
+                list[num] = "K"
+            if element == "14":
+                list[num] = "A"
+    return
 
-print("Player 1: "+ str(player_one))
-print("Player 2: "+ str(player_two))
-print("Board: FLOP = " + str(board[0:3])+", TURN = " + str(board[3:4]) + ", RIVER = " + str(board[4:5]))
-# count = 0
-# for key, value in deck.items():
-#     if isinstance(value, list):
-#         count += len(value)
+print("Player 1: "+ str(change_figures(player_one))) # str(player_one))
+print("Player 2: "+ str(change_figures(player_two)))
+print("Board: FLOP = " + str(change_figures(board[0:3]))+", TURN = " + str(change_figures(board[3:4])) + ", RIVER = " + str(change_figures(board[4:5])))
 
 
 # if __name__ == '__main__':
