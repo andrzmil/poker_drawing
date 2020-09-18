db1ce52af8f33af354cab3d00fdce18e5f990350
Andrzej Milewski <andrz.milewski@gmail.com>
2020-09-18 22:17:06 +0200

    added three of a kind and straight detection

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from collections import Counter
 players_count = 9
 
 #built card deck by using dictionary
-deck = {'2':['H','C','S','D'], '3':['H','C','S','D'], '4':['H','C','S','D'],
-        '5':['H','C','S','D'], '6':['H','C','S','D'], '7':['H','C','S','D'],
-        '8':['H','C','S','D'], '9':['H','C','S','D'], '10':['H','C','S','D'],
-        '11':['H','C','S','D'], '12':['H','C','S','D'], '13':['H','C','S','D'],
-        '14':['H','C','S','D']}
+deck = {2:['H','C','S','D'], 3:['H','C','S','D'], 4:['H','C','S','D'],
+        5:['H','C','S','D'], 6:['H','C','S','D'], 7:['H','C','S','D'],
+        8:['H','C','S','D'], 9:['H','C','S','D'], 10:['H','C','S','D'],
+        11:['H','C','S','D'], 12:['H','C','S','D'], 13:['H','C','S','D'],
+        14:['H','C','S','D']}
 
 def drawing_process():
     #defines how many numbers are currently in deck
@@ -16,7 +16,7 @@ def drawing_process():
 
     #draw number which represent number in deck
     def random_number():
-        return str(np.random.randint(2, len(numbers)+2))
+        return np.random.randint(2, len(numbers)+2)
 
     #try to draw figure
     chosen_number = None
@@ -40,7 +40,7 @@ def drawing_process():
             print(Exception)
 
     chosen_color = deck.get(chosen_number)[drawn_color]
-    deck[str(chosen_number)].remove(chosen_color)
+    deck[chosen_number].remove(chosen_color)
 
     return [chosen_number, chosen_color]
 
@@ -76,11 +76,11 @@ board = [drawing_process(), drawing_process(), drawing_process(), drawing_proces
 
 def evaluate_cards(player_cards):
     board_and_player = board + player_cards
+    board_and_player.sort(key=lambda x: x[0], reverse=True)
     bap_zip = list(zip(*board_and_player))
-    bap_sorted = sorted(list(bap_zip), key = lambda x: x[0])
 
-    print(bap_sorted)
-    print(res)
+
+
     figures = list(bap_zip[0])
     colors = list(bap_zip[1])
 
@@ -92,24 +92,45 @@ def evaluate_cards(player_cards):
     def col_freq():
         return Counter(colors)
 
-    def one_pair():
-        one_pair_check = [key for (key, value) in fig_freq().items() if value == 2]
-        if len(one_pair_check) == 1:
-            return [2,one_pair_check[0], figures.sort(reverse=True)]
+    def straight():
+        low_straight = [2,3,4,5,14]
+        for i in range(3):
+            if figures[i] == figures[i+4]+4:
+                return [5, figures[i:i+5], figures, sum(figures[i:i+5])]
+
+    def three_of_a_kind():
+        three_of_a_kind_check = [key for (key, value) in fig_freq().items() if value == 3]
+        if len(three_of_a_kind_check) == 1:
+            return [4, three_of_a_kind_check[0], figures[:5], sum(figures[:5])]
+        elif len(three_of_a_kind_check) == 2:
+            return [4, three_of_a_kind_check[0], figures[:5], sum(figures[:5])]
 
     def two_pairs():
         two_pairs_check = [key for (key, value) in fig_freq().items() if value == 2]
         if len(two_pairs_check) == 2:
-            return [3, [two_pairs_check[0],two_pairs_check[1]], figures.sort(reverse=True)]
+            return [3, [two_pairs_check[0],two_pairs_check[1]], figures[:5]]
         elif len(two_pairs_check) == 3:
-            return [3, [two_pairs_check[0],two_pairs_check[1],two_pairs_check[2]], figures.sort(reverse=True)]
+            return [3, [two_pairs_check[0],two_pairs_check[1],two_pairs_check[2]], figures[:5], sum(figures[:5])]
 
+    def one_pair():
+        one_pair_check = [key for (key, value) in fig_freq().items() if value == 2]
+        if len(one_pair_check) == 1:
+            return [2,one_pair_check[0], figures[:5],sum(figures[:5])]
 
+    def high_card():
+        return [1, figures, figures[:5], sum(figures[:5])]
+
+    print(straight())
+    print(three_of_a_kind())
     print(two_pairs())
     print(one_pair())
+    print(high_card())
     print(col_freq())
+    return board_and_player
 
-print(evaluate_cards(players[0].cards))
+
+for i in range(players_count):
+    print(evaluate_cards(players[i].cards))
 
 
 
